d04eb116b4ec2f4d7286ac1dabeada15266a43ed
FerTarallo <contato.fernandatarallo@gmail.com>
2022-10-07 19:55:15 -0300

    deleta o produto por id

--- src/routers/products.py
+++ src/routers/products.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, status
-from src.cruds.product import create_product
+from src.cruds.product import create_product, delete_product
 from src.schemas.products import ProductSchema
 from starlette.responses import JSONResponse
+
+from src.utils import parse_json
 
 router = APIRouter(tags=['Products'], prefix='/products')
 
@@ -9,3 +11,9 @@ router = APIRouter(tags=['Products'], prefix='/products')
 async def create_new_product(product: ProductSchema):
     product = await create_product(product)
     return JSONResponse(content={ 'data': {'product_id': str(product.inserted_id)}},status_code=status.HTTP_200_OK)
+
+
+@router.delete("/{id}")
+async def delete_product_id(id: str):
+    deleted_product = await delete_product(id)
+    return JSONResponse(content={ 'data': {'product_id': id}},status_code=status.HTTP_200_OK)
--- src/schemas/products.py
+++ src/schemas/products.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 
 class ProductSchema(BaseModel):
-    name: str = Field(unique=True)
+    name: str = Field(unique=True) #O unique está permitindo o cadastro de campos com o mesmo valor
     description: str
     price: float = Field(gt=0.01)
     inventory: int = Field(gt=0)
