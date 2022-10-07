b5146169e552dd3d8917a36fb3888323cef64dc9
FerTarallo <contato.fernandatarallo@gmail.com>
2022-10-06 21:10:33 -0300

    adiciona cadastro de produtos

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from src.routers import users
+from src.routers import users, products
 
 api_router = APIRouter()
 
 api_router.include_router(users.router)
+api_router.include_router(products.router)
