8142433b843935c50f57e2436a7b81dec26ef6a8
Vanessa Lima <vanessalima951@gmail.com>
2022-10-08 12:16:48 -0300

    organização dos métodos do endereço e implementando exceções

--- src/cruds/address.py
+++ src/cruds/address.py
@@ -1,9 +1,12 @@
+import logging
 from fastapi import status, HTTPException
 from src.schemas.address import Address
 from bson.objectid import ObjectId
 from src.server.database import db
 
+logger = logging.getLogger(__name__)
 
+# TODO será deletado assim que o método de get user for implementado
 user = {
     "_id": ObjectId("632f955d05f8a6b497416823"),
     "email": "vanessa@gmail.com",
@@ -13,37 +16,21 @@ user = {
 }
 
 async def get_addresses(user_id: str):
-    try:
-        user_address = await db.addresses_db.find_one({'user._id': ObjectId(user_id)})
-        
-        if user_address:
-            return user_address
-        
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Usuário não encontrados')
-    
-    except Exception as e:
-        print(f'get_address.error: {e}')
+    address_list = await db.addresses_db.find_one({'user._id': ObjectId(user_id)})
+    if not address_list:
+        return []
+    return address_list
 
 
 async def create_address(user_id: str, address: Address):
-    try:
-        user_address = await get_addresses(user_id)
-        address = address.dict()
-        address['_id'] = ObjectId()
-        
-        if user_address:
-            user_address = await db.addresses_db.update_one(
-                {'_id': user_address['_id']},
-                {
-                    '$addToSet': {
-                        'address': dict(sorted(address.items()))
-                    }
-                }
-            )
-            
-            if user_address.modified_count:
-                return {'Endereços adicionados': user_address.modified_count}
-        
+    # TODO verificar se o usuário existe com o método get user by id
+    if not user:
+        raise HTTPException(detail='Usuário não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+    user_address = await get_addresses(user_id)
+    address = address.dict()
+    address['_id'] = ObjectId()
+    
+    if not user_address:
         user_address = await db.addresses_db.insert_one(
             {
                 'user': user,
@@ -54,49 +41,52 @@ async def create_address(user_id: str, address: Address):
         )
         if user_address.inserted_id:
             return 'Endereço criado'
-
-    except Exception as e:
-        print(f'create_address.error: {e}')
-
+        
+    user_address = await db.addresses_db.update_one(
+        {'_id': user_address['_id']},
+        {
+            '$addToSet': {
+                'address': dict(sorted(address.items()))
+            }
+        }
+    )
+    
+    if user_address.modified_count:
+        return {'Endereços adicionados': user_address.modified_count}
+        
 
 async def find_address_by_id(user_id: str, address_id: str):
-    try:
-        user_address = await get_addresses(user_id)
+    # TODO verificar se o usuário existe com o método get user by id
+    if not user:
+        raise HTTPException(detail='Usuário não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+    user_address = await get_addresses(user_id)
+    if not user_address:
+            raise HTTPException(detail='Nenhum endereço encontrado para este usuário', status_code=status.HTTP_404_NOT_FOUND)
+    address_list = user_address['address']
+    address = [v for v in address_list if v['_id'] == ObjectId(address_id)]
+    if not address:
+        raise HTTPException(detail='Endereço não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+    return address
         
-        if user_address:
-            address_list = user_address['address']
-            address = [v for v in address_list if v['_id'] == ObjectId(address_id)]
-            if address:
-                return address
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Endereço {address_id} não encontrado')
         
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Usuário ou endereço {address_id} não encontrados')
-        
-    except Exception as e:
-        print(f'get_address_by_id.error: {e}')
-        
-
 async def remove_address_by_id(user_id: str, address_id: str):
-    try:
-        user_address = await get_addresses(user_id)
-        
-        if user_address:
-            address_list = user_address['address']
-            updated_address_list = [v for v in address_list if v['_id'] != ObjectId(address_id)]
-            address_list = await db.addresses_db.update_one(
-                {'_id': user_address['_id']},
-                {
-                    '$set': {
-                        'address': updated_address_list
-                    }
-                }
-            )
-            
-            if address_list.modified_count:
-                return 'Endereço removido'
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Endereço {address_id} não encontrado')
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Usuário ou endereço {address_id} não encontrados')
-            
-    except Exception as e:
-        print(f'remove_address_by_id.error: {e}')
-        
+    # TODO verificar se o usuário existe com o método get user by id
+    if not user:
+        raise HTTPException(detail='Usuário não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+    user_address = await get_addresses(user_id)
+    if not user_address:
+        raise HTTPException(detail='Nenhum endereço encontrado para este usuário', status_code=status.HTTP_404_NOT_FOUND)
+    
+    address_list = user_address['address']
+    address = [v for v in address_list if v['_id'] == ObjectId(address_id)]
+    if len(address) < 1:
+        raise HTTPException(detail='Endereço não encontrado', status_code=status.HTTP_404_NOT_FOUND)
+    updated_address_list = [v for v in address_list if v['_id'] != ObjectId(address_id)]
+    updated_address_list = await db.addresses_db.update_one(
+        {'_id': user_address['_id']},
+        {'$set': {'address': updated_address_list}}
+    )
+    if updated_address_list.modified_count:
+        return {}
+                
+          
--- src/routers/address.py
+++ src/routers/address.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status
-from starlette.responses import JSONResponse
+from starlette.responses import JSONResponse, Response
 from src.cruds.address import create_address, get_addresses, find_address_by_id, remove_address_by_id
 from src.schemas.address import Address
 from src.utils import parse_json
@@ -10,22 +10,22 @@ router = APIRouter(tags=["Address"], prefix="/user/{user_id}/address")
 @router.post("/")
 async def create_new_address(address: Address, user_id: str):
     address = await create_address(user_id, address)
-    return JSONResponse(content={'data': address}, status_code=status.HTTP_201_CREATED)
+    return JSONResponse(content={'message': address}, status_code=status.HTTP_201_CREATED)
 
 
 @router.get("/")
 async def get_address(user_id: str):
     address_list = await get_addresses(user_id)
-    return JSONResponse(content={'data': {'user_address': parse_json(address_list)}}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={'address_list': parse_json(address_list)}, status_code=status.HTTP_200_OK)
 
 
 @router.get("/{address_id}")
 async def get_address_by_id(user_id: str, address_id: str):
     address = await find_address_by_id(user_id, address_id)
-    return JSONResponse(content={'data': {'address': parse_json(address)}}, status_code=status.HTTP_200_OK)
+    return JSONResponse(content={'address': parse_json(address)}, status_code=status.HTTP_200_OK)
 
 
 @router.delete("/{address_id}")
 async def delete_address_by_id(user_id: str, address_id: str):
-    response = await remove_address_by_id(user_id, address_id)
-    return JSONResponse(content={'data': {'address': response}}, status_code=status.HTTP_200_OK)
+    await remove_address_by_id(user_id, address_id)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
