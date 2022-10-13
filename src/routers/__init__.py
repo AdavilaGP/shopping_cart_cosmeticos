a09da3a67a655730bf74e9a9e9a07f3104691830
FerTarallo <contato.fernandatarallo@gmail.com>
2022-10-12 22:30:34 -0300

    chore: welcome route

--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from src.routers import users, address, products, order
+from src.routers import users, address, products, order, root
 
 api_router = APIRouter()
 
+api_router.include_router(root.router)
 api_router.include_router(users.router)
 api_router.include_router(products.router)
 api_router.include_router(address.router)
